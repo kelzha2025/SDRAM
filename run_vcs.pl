98696225706fd1d553f1c5f970bbf7bf92426ddb
kelzha2025 <littlelijia2025@gmail.com>
2025-12-27 15:14:48 +0800

    init

---
+++ run_vcs.pl
@@ -0,0 +1,81 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Getopt::Long;
+
+# Simple VCS + Verdi runner for the UVM version of SDRAM verification
+# Usage examples:
+#   perl run_vcs.pl                # compile + run, UVM 1.2, no FSDB
+#   perl run_vcs.pl --fsdb         # compile + run, generate FSDB
+#   perl run_vcs.pl --clean        # remove previous build artifacts
+# Options can be combined; --dry-run prints commands without executing.
+
+my $fsdb      = 0;
+my $clean     = 0;
+my $dry_run   = 0;
+my $uvm_test  = 'test';
+my $vcs_home  = $ENV{VCS_HOME}    || '/home/synopsys/vcs/O-2018.09-SP2';
+my $verdi_home= $ENV{VERDI_HOME}  || '/home/synopsys/verdi/Verdi_O-2018.09-SP2';
+
+GetOptions(
+    'fsdb!'      => \$fsdb,
+    'clean!'     => \$clean,
+    'dry-run!'   => \$dry_run,
+    'uvm-test=s' => \$uvm_test,
+) or die "Error in command line arguments\n";
+
+my $vcs_bin   = "$vcs_home/bin/vcs";
+my $simv      = './simv';
+my $comp_log  = 'comp.log';
+my $sim_log   = 'sim.log';
+my $fsdb_def  = $fsdb ? '+define+FSDB' : '';
+
+my @clean_list = (
+    'csrc', 'simv', 'simv.daidir', 'ucli.key', 'vcs.key', 'vc_hdrs.h',
+    'sim.log', 'comp.log', 'DVEfiles', 'urgReport', 'vdb',
+);
+
+if ($clean) {
+    for my $path (@clean_list) {
+        run_cmd("rm -rf $path");
+    }
+    exit 0;
+}
+
+my @vcs_cmd = (
+    $vcs_bin,
+    '-full64',
+    '-sverilog',
+    '-timescale=1ns/1ps',
+    '-ntb_opts', 'uvm-1.2',
+    '-f', 'dut.f',
+    '-f', 'tb.f',
+    '-top', 'my_top',
+    '-debug_access+all',
+    '-P', "$verdi_home/share/PLI/VCS/LINUX64/novas.tab",
+          "$verdi_home/share/PLI/VCS/LINUX64/pli.a",
+    ($fsdb_def ? $fsdb_def : ()),
+    '-l', $comp_log,
+);
+
+my @sim_cmd = (
+    $simv,
+    '+UVM_TESTNAME=' . $uvm_test,
+    ($fsdb_def ? $fsdb_def : ()),
+    '-l', $sim_log,
+);
+
+run_cmd(join(' ', @vcs_cmd));
+run_cmd(join(' ', @sim_cmd));
+
+exit 0;
+
+sub run_cmd {
+    my ($cmd) = @_;
+    if ($dry_run) {
+        print "[DRY-RUN] $cmd\n";
+        return;
+    }
+    print "$cmd\n";
+    system($cmd) == 0 or die "Command failed: $cmd\n";
+}
