71b2c6ebbd49c90e8ba03c634059fdabf21c7929
kelzha2025 <littlelijia2025@gmail.com>
2025-12-27 16:04:07 +0800

    fix SV-ACCNBI

--- tb_classes/env.svh
+++ tb_classes/env.svh
@@ -12,13 +12,14 @@ class env extends uvm_env;
    `uvm_component_utils(env);
 
    stimulus_tester   tester_h;
-   coverage      coverage_h;
-   scoreboard    scoreboard_h;
+   coverage          coverage_h;
+   scoreboard        scoreboard_h;
 
    function void build_phase(uvm_phase phase);
-      tester_h     = stimulus_tester::type_id::create("tester_h",this);
-      coverage_h   = coverage::type_id::create ("coverage_h",this);
-      scoreboard_h = scoreboard::type_id::create("scoreboard_h",this);
+      // Instantiate the concrete stimulus implementation to avoid creating the abstract base class
+      tester_h     = stimulus_values::type_id::create("tester_h", this);
+      coverage_h   = coverage::type_id::create("coverage_h", this);
+      scoreboard_h = scoreboard::type_id::create("scoreboard_h", this);
    endfunction : build_phase
 
    function new (string name, uvm_component parent);
